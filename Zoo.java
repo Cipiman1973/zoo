b83c9f996d809f8ccdb56a344e3ff680be976556
Cipiman1973 <62877816+Cipiman1973@users.noreply.github.com>
2021-01-15 22:03:44 +0100

    Update Zoo.java

--- Zoo.java
+++ Zoo.java
@@ -1,40 +1,75 @@
-@Path("animal")
+import javax.ws.rs.*;
 
-public class ZOO {
+import javax.ws.rs.core.MediaType;
 
-    public String[] animals;
+import javax.ws.rs.core.Response;
 
-    @GET
+import java.util.HashMap;
 
-    public Response getanim(@DefaultValue("-2147483648") @FormParam("id") int pos){
+@Path("animals") 
 
-        if (pos == -2147483648)
+@Produces(MediaType.APPLICATION_JSON)
 
-            return Response.ok(Arrays.toString(animals)).build();
+public class UsersResource {
 
-        return Response.ok(animals[pos]).build();
-    }
-    @PUT
+	static HashMap<Integer, HashMap<String, String>> animals = new HashMap<>();
+    
+	static Integer index = 0;
 
-    public Response addanim(@FormParam("animal") String name, @FormParam("id") int pos){
+	@GET
+    
+	public Response getAnimals(){
+        
+		return Response.ok(animals).build();
+	}
 
-        if (animals[pos] != null)
+	@POST
+	public Response giveBirthToAnAnimal(@FormParam("jmeno") String name, @FormParam("vek") String age) {
+		if(name != null && age != null ){
+			HashMap<String,String> newAnimal = new HashMap<>();
+			newAnimal.put("jmeno", name);
+			newAnimal.put("vek", age);
+			animals.put(index, newAnimal);
+			index++;
+			return Response.ok(animals).build();
+		} else {
+			return Response.ok("VLOZTE PARAMETR").build();
+		}
+	}
 
-            return Response.status(400).build();
+	@GET
+	@Path("{index}")
+	public Response getAnimal(@PathParam("index") Integer index) {
+		if(index != null && animals.get(index) != null){
+			return Response.ok(animals.get(index)).build();
+		} else {
+			return Response.ok("NEEXISTUJE").build();
+		}
+	}
 
-        animals[pos] = name;
+	@DELETE
+	@Path("{index}")
+	public Response slaughterAnimal(@PathParam("index") Integer index) {
+		if(index != null){
+			animals.remove(index);
+		}
+		return Response.ok(animals).build();
+	}
 
-        return Response.ok().build();
+	@PUT
+	@Path("{index}")
+	public Response changeAge(@PathParam("index") Integer index,@FormParam("jmeno") String name ,@FormParam("vek") String age) {
+		if(animals.get(index) != null && index != null && name != null && age != null){
+			HashMap<String,String> newAnimal = new HashMap<>();
+			newAnimal.put("jmeno", name);
+			newAnimal.put("vek", age);
+			animals.put(index, newAnimal);
+		}
+		return Response.ok(animals).build();
+	}
 
-    }
-    @DELETE
-
-    public Response remanim(@FormParam("id") int pos){
-
-        animals[pos] = null;
 
 
-        return Response.ok().build();
 
-    }
+
 }
